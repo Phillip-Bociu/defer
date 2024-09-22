9d05be9f159ecfdd4b4ec51d70e78ea225dc0131
Phillip-Bociu <dillymcbilly101@gmail.com>
2024-09-22 13:59:50 +0300

    fixed tests

--- test.cpp
+++ test.cpp
@@ -6,15 +6,26 @@ int main() {
 	{
 		defer {
 			a++;
+			defer {
+				a++;
+			};
 		};
-		if(a != 0) {
-			std::cout << "fail 1" << std::endl;
+		{
+			defer {
+				a++;
+			};
+			if(a != 0) {
+				std::cout << "fail 1" << std::endl;
+				return -1;
+			}
+		}
+		if(a != 1) {
+			std::cout << "fail 2" << std::endl;
 			return -1;
 		}
 	}
-
-	if(a != -1) {
-		std::cout << "fail 2" << std::endl;
+	if(a != 3) {
+		std::cout << "fail 3" << std::endl;
 		return -1;
 	}
 
